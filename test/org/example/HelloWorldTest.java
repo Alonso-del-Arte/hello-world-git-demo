2a6661611c044054c8aff26ff0a570a861772a09
Alonso del Arte <alonso.delarte@gmail.com>
2023-12-20 20:59:07 -0500

    Failing greeting( ) test

--- test/org/example/HelloWorldTest.java
+++ test/org/example/HelloWorldTest.java
@@ -6,7 +6,12 @@ package org.example;
 
 import java.io.ByteArrayOutputStream;
 import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Locale;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -17,22 +22,31 @@ import static org.junit.Assert.*;
  */
 public class HelloWorldTest {
     
+    private static final List<Locale> AVAILABLE_LOCALES 
+            = Arrays.asList(Locale.getAvailableLocales());
+    
+    private static List<Locale> getByISO3Language(String iso3Tag) {
+        return AVAILABLE_LOCALES.stream().filter(loc 
+                -> loc.getISO3Language().equals(iso3Tag))
+                .collect(Collectors.toList());
+    }
+    
     /**
-     * Test of greeting method, of class HelloWorld.
+     * Test of the greeting function, of the HelloWorld class.
      */
     @Test
     public void testGreeting() {
         System.out.println("greeting");
-        Locale locale = null;
-        String expResult = "";
-        String result = HelloWorld.greeting(locale);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        List<Locale> locales = getByISO3Language("eng");
+        String expected = "Hello, world!";
+        for (Locale locale : locales) {
+            String actual = HelloWorld.greeting(locale);
+            assertEquals(expected, actual);
+        }
     }
 
     /**
-     * Test of main method, of class HelloWorld.
+     * Test of the main procedure, of the HelloWorld class.
      */
     @Test
     public void testMain() {

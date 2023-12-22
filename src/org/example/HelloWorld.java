0fd29cd9e969257b6205350c0f35286400176df3
Alonso del Arte <alonso.delarte@gmail.com>
2023-12-21 19:57:56 -0500

    Added Chinese, Japanese, Korean; passing

--- src/org/example/HelloWorld.java
+++ src/org/example/HelloWorld.java
@@ -12,13 +12,19 @@ import java.util.Locale;
  */
 public class HelloWorld {
     
+    private static final String KOREAN_GREETING 
+            = "\uC548\uB155\uD558\uC138\uC694, \uC138\uACC4\uC785\uB2C8\uB2E4!";
+    
     public static String greeting(Locale locale) {
         return switch (locale.getISO3Language()) {
             case "deu" -> "Hallo Welt!";
             case "eng" -> "Hello, world!";
             case "fra" -> "Bonjour le monde!";
             case "ita" -> "Ciao mondo!";
+            case "jpn" -> "\u3053\u3093\u306B\u3061\u306F\u4E16\u754C\uFF01";
+            case "kor" -> KOREAN_GREETING;
             case "spa" -> "\u00A1Hola, mundo!";
+            case "zho" -> "\u4F60\u597D\u4E16\u754C\uFF01";
             default -> "SORRY, NOT IMPLEMENTED YET";
         };
     }

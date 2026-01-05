33f6007c902a18e8267afcde1f580e33719939ed
Alonso del Arte <alonso.delarte@gmail.com>
2026-01-05 15:01:23 -0500

    Removed test that is no longer applicable because main greeting( ) test has been generalized to all locales

--- test/org/example/HelloWorldTest.java
+++ test/org/example/HelloWorldTest.java
@@ -96,32 +96,6 @@ public class HelloWorldTest {
     // TODO: Rewrite all the following tests
     
     /**
-     * Another test of the greeting function, of the HelloWorld class. For 
-     * non-English-speaking locales, such as for Spanish and German, we might be 
-     * able to provide other greetings.
-     */
-    @Test
-    public void testGreetingOtherLocaleLanguages() {
-        String koreanGreeting = "\uC548\uB155\uD558\uC138\uC694, " 
-                + "\uC138\uACC4\uC785\uB2C8\uB2E4!";
-        Map<String, String> expGreetings = Map.of("deu", "Hallo Welt!", 
-                "fra", "Bonjour le monde!", "ita", "Ciao mondo!", 
-                "jpn", "\u3053\u3093\u306B\u3061\u306F\u4E16\u754C\uFF01", 
-                "kor", koreanGreeting, "por", "Ol\u00E1 mundo!", 
-                "spa", "\u00A1Hola, mundo!", 
-                "zho", "\u4F60\u597D\u4E16\u754C\uFF01");
-        expGreetings.forEach((iso3Tag, expected) -> {
-            List<Locale> locales = getByISO3Language(iso3Tag);
-            for (Locale locale : locales) {
-                String actual = HelloWorld.greeting(locale);
-                String message = "Greeting for " + locale.getDisplayName() 
-                        + " expected to be \"" + expected + "\"";
-                assertEquals(message, expected, actual);
-            }
-        });
-    }
-
-    /**
      * Test of the main procedure, of the HelloWorld class.
      */
     @Test
